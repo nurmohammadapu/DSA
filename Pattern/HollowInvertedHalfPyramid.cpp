f35f1cee16f60a44522432f7810c4bcff6444cef
Nur Mohammad Apu <mdapu2003@gmail.com>
2025-04-11 23:40:28 +0600

    Add HollowInvertedHalfPyramid pattern program

---
+++ Pattern/HollowInvertedHalfPyramid.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n;
+  cout<<"Enter your number:\n";
+  cin>>n;
+
+  for(int row=0; row<n; row++){
+    for(int col=0; col<n-row; col++){
+      if(row==0 || col==0 || col==n-row-1){
+        cout<<"*";
+      }
+      else{
+        cout<<" ";
+      }
+    }
+    cout<<endl;
+  }
+
+}
+
+
+/*
+output:
+Enter your number:
+5
+*****
+*  *
+* *
+**
+*
+
+*/
